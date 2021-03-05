a9ec4d000e8cd2f8e18f52600273dc93b724a364
Nidai Çağrı Savran <nsavran19@ku.edu.tr>
2021-03-05 10:36:41 +0000

    some corrections

--- ps1a.py
+++ ps1a.py
@@ -2,20 +2,21 @@ n, B = list(map(int, input().strip().split()))
 T = 0
 
 # your code here
-tries = 0
 result = 0
-for i in range(1, n+1):
-  if i%2 ==0:
+for i in range(1, n + 1):
+  if i % 2 ==0:
     p = 2 ** i + 1
   else:
     p = 3 ** i + 1
   result += p ** (n - i)
 while T < 10000:
-  T +=1
-  if result*T >B:
+  T += 1
+  if result * T >B:
     break
 
-if tries == 10000:
+if T == 10000 and result * T >B:
+  T = 10000
+if T == 10000 and result * T <B:
   T = -1
 # please do not modify the input and print statements
 # and make sure that your code does not have any other print statements
--- ps1b.py
+++ ps1b.py
@@ -6,8 +6,8 @@ i = 0
 result_small = 0
 t = 1
 T = 10000
-for i in range(1, n+1):
-  if i%2 ==0:
+for i in range(1, n + 1):
+  if i % 2 == 0:
     p = 2 ** i + 1
   else:
     p = 3 ** i + 1
@@ -19,15 +19,15 @@ if result_small > B:
   T = 1
 else:
   while i < 10000:
-    t_temporary = (t+T)//2
-    if t_temporary == t or t_temporary ==T:
+    t_temporary = (t + T) // 2
+    if t_temporary == t or t_temporary == T:
       break
     temp_result = result_small * t_temporary
     if temp_result > B:
       T = t_temporary
     else:
       t = t_temporary
-    i+=1
+    i += 1
 # please do not modify the input and print statements
 # and make sure that your code does not have any other print statements
 print(T)
