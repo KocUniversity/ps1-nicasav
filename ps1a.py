ff38d616710be53cd033c1636238d6c3b5400531
Nidai Çağrı Savran <nsavran19@ku.edu.tr>
2021-03-05 10:31:49 +0000

    Solutions to examples

---
+++ ps1a.py
@@ -0,0 +1,22 @@
+n, B = list(map(int, input().strip().split()))
+T = 0
+
+# your code here
+tries = 0
+result = 0
+for i in range(1, n+1):
+  if i%2 ==0:
+    p = 2 ** i + 1
+  else:
+    p = 3 ** i + 1
+  result += p ** (n - i)
+while T < 10000:
+  T +=1
+  if result*T >B:
+    break
+
+if tries == 10000:
+  T = -1
+# please do not modify the input and print statements
+# and make sure that your code does not have any other print statements
+print(T)
